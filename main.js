fea207d45f68a9b29f4468b706f68bb93295c156
Terence Huynh <me@terencehuynh.com>
2018-10-16 22:41:15 +1100

    More changes

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 const ready = () => {
   // package selector for mobile
   const buttons = Array.from(
-    document.querySelectorAll('.pkg-mobile-selector__button'),
+    document.querySelectorAll(".pkg-mobile-selector__button")
   );
-  const container = document.querySelector('.pkg-container');
+  const container = document.querySelector(".pkg-container");
 
   buttons.map(button => {
-    button.addEventListener('click', event => {
+    button.addEventListener("click", event => {
       const package = event.target.dataset.package;
       container.dataset.package = package;
     });
@@ -14,26 +14,26 @@ const ready = () => {
 
   // info button in table
   const infoButtons = Array.from(
-    document.querySelectorAll('.pkg-table__more-info'),
+    document.querySelectorAll(".pkg-table__more-info")
   );
   infoButtons.map(button => {
-    button.addEventListener('click', event => {
-      event.target.classList.toggle('active');
+    button.addEventListener("click", event => {
+      event.target.classList.toggle("active");
     });
   });
 
-  const selector = document.querySelector('.pkg-mobile-selector');
-  const table = document.querySelector('.pkg-table');
+  const selector = document.querySelector(".pkg-mobile-selector");
+  const table = document.querySelector(".pkg-table");
   const scrollListener = event => {
     const rect = table.getBoundingClientRect();
     if (rect.top < -50 && rect.bottom > 50) {
-      selector.classList.add('sticky');
+      selector.classList.add("sticky");
     } else {
-      selector.classList.remove('sticky');
+      selector.classList.remove("sticky");
     }
   };
-  window.addEventListener('scroll', scrollListener, { passive: true });
+  window.addEventListener("scroll", scrollListener, { passive: true });
   scrollListener();
 };
 
-document.addEventListener('DOMContentLoaded', ready);
+document.addEventListener("DOMContentLoaded", ready);
